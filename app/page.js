7dda92ec58e21ab50f9c16e4c59e58ab75e65565
kimkim168 <KimKim168>
2024-11-18 16:34:02 +0700

    update

--- app/page.js
+++ app/page.js
@@ -65,52 +65,54 @@ export default function Home() {
     <header className="mt-3 sm:px-2 sm:pt-8 flex items-center justify-between gap-4  ">
       {/* Logo */}
 
-      <div className="gap-4 grid grid-cols-12 justify-center items-center">
+      <div className="gap-4 flex w-full justify-center items-center">
         {/* Mobile Menu Toggle */}
-        <ShadCNButton variant="outline" size="icon" className='shrink-0'>
-          <MenuIcon/>
+        <ShadCNButton variant="outline" size="icon" className="shrink-0">
+          <MenuIcon />
         </ShadCNButton>
         {/* Search Bar */}
-        <form className="max-w-xl col-span-11 relative ">
-          <label
-            for="default-search"
-            className=" text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >
-            Search
-          </label>
-          <div className="relative">
-            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg
-                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                ariaHidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M19 19l-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
-            <input
-              type="search"
-              id="default-search"
-              className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search Product..."
-              required
-            />
-            <button
-              type="submit"
-              className="text-white absolute end-1.5 bottom-1.5 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <div className="flex-1">
+          <form className="w-full relative ">
+            <label
+              for="default-search"
+              className=" text-sm font-medium text-gray-900 sr-only dark:text-white"
             >
               Search
-            </button>
-          </div>
-        </form>
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                  ariaHidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 19l-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Product..."
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute end-1.5 bottom-1.5 bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
         <div
           className="items-center justify-between hidden w-full  md:w-auto md:order-1"
           id="navbar-sticky"
@@ -153,9 +155,9 @@ export default function Home() {
         </div>
       </div>
 
-      <ShadCNButton variant="outline" size="icon" className='shrink-0' >
-          <SlidersHorizontal />
-        </ShadCNButton>
+      <ShadCNButton variant="outline" size="icon" className="shrink-0">
+        <SlidersHorizontal />
+      </ShadCNButton>
     </header>
   );
 
