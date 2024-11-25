26231ad60ffc0cf498fcbec6bb3084bc531c24f7
kimkim168 <KimKim168>
2024-11-25 15:47:10 +0700

    updat

--- app/client/classification/page.js
+++ app/client/classification/page.js
@@ -16,68 +16,70 @@ export default function page() {
             DDC | LC (AL Power)
           </h1>
         </div>
-        {/* Search */}
-        {/* <div className="mt-10 max-w-screen-2xl mx-auto">
-          <form>
-            <label
-              htmlFor="default-search"
-              className="text-sm font-medium text-gray-900 sr-only dark:text-white"
-            >
-              Search
-            </label>
-            <div className="relative">
-              <div
-                className="absolute inset-y-0 start-0 flex items-center ps-3"
-                aria-hidden="true"
-              >
-                <svg
-                  className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M19 19l-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                  />
-                </svg>
-              </div>
-              <input
-                type="search"
-                id="default-search"
-                className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                placeholder="Search ...."
-                required
-                aria-label="Search"
-              />
-              <button
-                type="submit"
-                className="text-white absolute end-1.5 bottom-1.5 bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-4 py-2"
-              >
-                Search
-              </button>
-            </div>
-          </form>
-        </div> */}
-        {/* End Search */}
+
+        <hr className="border-black my-7"></hr>
 
         {/* Start */}
-        <div className="grid  grid-cols-12 max-w-screen-2xl gap-4 mx-auto  mt-6">
-          <div className="col-span-12 sm:col-span-8 border borderColor  rounded-lg bg-white p-6 shadow-md w-full">
+        <div className="grid  grid-cols-12 max-w-screen-2xl gap-4 mx-auto  mt-5">
+          <div className="col-span-12 sm:col-span-8 border borderColor  rounded-lg bg-white p-6  w-full">
             <h1 className="text-[16px] sm:text-xl md:text-xl font-normal text-black mb-2">
               Standard Number Khmer Book
             </h1>
             <p className="text-sm text-black mb-4">
               Enter an ISBN, ISSN or TSIN (Thnal Standard Identification Number)
             </p>
-            <input
+            {/* <input
               type="text"
               placeholder="Enter number here"
               className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-0.5 focus:ring-black focus:border-black"
-            />
+            /> */}
+            {/* Search */}
+            <div>
+              <form>
+                <label
+                  htmlFor="default-search"
+                  className="text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div className="relative">
+                  <div
+                    className="absolute inset-y-0 start-0 flex items-center ps-3"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 19l-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                  </div>
+                  <input
+                    type="search"
+                    id="default-search"
+                    className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    placeholder="Search ...."
+                    required
+                    aria-label="Search"
+                  />
+                  <button
+                    type="submit"
+                    className="text-white absolute end-1.5 bottom-1.5 bg-primary hover:bg-primary/90 focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-4 py-2"
+                  >
+                    Search
+                  </button>
+                </div>
+              </form>
+            </div>
+            {/* End Search */}
           </div>
           <div className="col-span-12 sm:col-span-4 bg-payMent rounded-lg text-white text-center p-6">
             <h1 className="text-sm sm:text-xl md:text-xl">
@@ -91,6 +93,7 @@ export default function page() {
             </h1>
           </div>
         </div>
+
         {/* End */}
         {/* Start */}
         <div className="max-w-screen-2xl mx-auto  mt-6 text-[16px] sm:text-[18px] md:text-[18px] space-y-3">
